ee366a1e1c1ff3ba88f2eaf767255d5ccbb409a7
bnimmo200 <72871290+bnimmo200@users.noreply.github.com>
2022-04-17 22:24:25 +0100

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/org/me/gcu/mpdcw_b_nimmo_s1802408/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.me.gcu.mpdcw_b_nimmo_s1802408;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.me.gcu.mpdcw_b_nimmo_s1802408";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
